b0960e0b14201570005df53d24547e8776e5d61e
Eric Andre <eric@meetidesign.com>
2015-08-04 18:32:44 -0500

    start fleshing out test spec

--- tests/paperboySpec.js
+++ tests/paperboySpec.js
@@ -1,19 +1,49 @@
 describe('paperboy', function() {
+  var mock_topic = 'news';
+  var mock_publish_data = 'published to this topic';
+  function mock_topic_callback(data) {
+    console.log(mock_topic + ': ' + data);
+  }
+
     it('should exist', function() {
         expect(paperboy).toBeDefined();
     });
     it('should be an Object', function() {
         expect(typeof paperboy === 'object').toEqual(true);
     });
-    it('should have a property topics', function() {
+    it('should have an own property topics', function() {
         expect(paperboy.hasOwnProperty('topics')).toEqual(true);
     });
-    describe('subscribe', function() {
+    describe('subscribe', function() { 
         it('should be defined', function() {
             expect(paperboy.subscribe).toBeDefined();
         });
-        it('should be a property of paperboy', function() {
+        it('should be an own property of paperboy', function() {
             expect(paperboy.hasOwnProperty('subscribe')).toEqual(true);
+        });
+        it('should register a topic and listener to topics', function() {
+            paperboy.subscribe(mock_topic, mock_topic_callback);
+            expect(paperboy.topics[mock_topic]).toBeDefined();
+            expect(paperboy.topics[mock_topic]).toContain(mock_topic_callback);
+        });
+    });
+    describe('publish', function() {
+        it('should be defined', function() {
+            expect(paperboy.publish).toBeDefined();
+        });
+        it('should be an own property of paperboy', function() {
+            expect(paperboy.hasOwnProperty('publish')).toEqual(true);
+        });
+        //it('should notify a topic with new data', function() {
+        //    expect(paperboy.publish([mock_topic], mock_publish_data)).toEqual(mock_topic + ': ' + mock_publish_data);
+        //});
+    });
+    describe('broadcast', function() {
+        it('should be defined', function() {
+            expect(paperboy.broadcast).toBeDefined();
+        });
+        it('should be a property of paperboy', function() {
+            expect(paperboy.hasOwnProperty('broadcast')).toEqual(true);
         });
     });
     describe('unscubscribe', function() {
@@ -22,22 +52,6 @@ describe('paperboy', function() {
         });
         it('should be a property of paperboy', function(){
             expect(paperboy.hasOwnProperty('unsubscribe')).toEqual(true);
-        });
-    });
-    describe('publish', function() {
-        it('should be defined', function() {
-            expect(paperboy.publish).toBeDefined();
-        });
-        it('should be a property of paperboy', function() {
-            expect(paperboy.hasOwnProperty('publish')).toEqual(true);
-        });
-    });
-    describe('broadcast', function() {
-        it('should be defined', function() {
-            expect(paperboy.broadcast).toBeDefined();
-        });
-        it('should be a property of paperboy', function() {
-            expect(paperboy.hasOwnProperty('broadcast')).toEqual(true);
         });
     });
 });
